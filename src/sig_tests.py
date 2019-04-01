a895196dc0ae13f295a1ba55a502261760615e25
chowes <root@colinhowes.com>
2019-04-01 16:45:09 -0400

    ANOVA and pairwise comparison functions

--- src/sig_tests.py
+++ src/sig_tests.py
@@ -27,6 +27,29 @@ def anova(data, d_var, i_var, categories):
     return stats.f_oneway(*samples)
 
 
+def pairwise(data, d_var, i_var, categories):
+    """
+    Perform pairwise comparisons
+
+    Args:
+        data: data frame
+        d_var: dependent variable
+        i_var: independent variable
+        categories: independent variable categories
+
+    Returns:
+        List of tuples of category, category, U value, p value
+    """
+    results = []
+    for i in range(len(categories) - 1):
+        for j in range(i+1, len(categories)):
+            u_val, p_val = stats.mannwhitneyu(
+                    data[data[i_var] == categories[i]][d_var],
+                    data[data[i_var] == categories[j]][d_var])
+            results.append((categories[i], categories[j], u_val, p_val))
+    return results
+
+
 def main(argv):
     """
     Args:
@@ -41,10 +64,17 @@ def main(argv):
     data = pandas.read_csv(input_file)
 
     # ANOVA issue type -> number of revisions
+    print("issue type -> number of revisions")
     f_value, p_value = anova(
             data, 'num_revisions', 'final_category', ISSUE_TYPES)
-    print("ANOVA for issue type -> number of revisions: F={}, p={}".format(
-        f_value, p_value))
+    results = pairwise(
+            data, 'num_revisions', 'final_category', ISSUE_TYPES)
+
+    print("\tANOVA:")
+    print("\t\tF={}, p={}".format(f_value, p_value))
+    print("\tPairwise Comparisons:")
+    for r in results:
+        print("\t\t{} {} U={}, p={}".format(r[0], r[1], r[2], r[3]))
 
 
 if __name__ == "__main__":
